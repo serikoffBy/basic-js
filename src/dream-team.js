f936f33ef05179365e1538ae88c3dca767601c00
serikoffBy <60609970+serikoffBy@users.noreply.github.com>
2020-10-02 16:42:42 +0300

    Add files via upload

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let result = '';
+  members.forEach(element => {
+    if(typeof(element) === "string") {
+      result = result + element.trim()[0].toUpperCase();
+    }
+  });
+  return result.sort();
 };
