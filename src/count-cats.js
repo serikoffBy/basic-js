c6adb4ad2e9d4024bec6f80ade4a6553a72f70e5
serikoffBy <60609970+serikoffBy@users.noreply.github.com>
2020-10-02 10:54:20 +0300

    Add files via upload

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,14 +2,11 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
   let sum = 0;
+  if (matrix.length === 0) return 0;
   matrix.forEach(element => {
-    if (typeof(element) === Array) {
-      element.forEach(elementSecond => {
-        if (elementSecond.includes('^^')) sum = sum + 1;
-      })
-    } else {
-        if (element.includes('^^')) sum = sum + 1;
-    }
+    if(typeof(element) === Array) {
+      if (element.includes('^^')) sum = sum + 1;
+    } 
   });
   return sum;
 };
