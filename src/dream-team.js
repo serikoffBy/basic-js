cafb90218c50c2a0fa6e40effeee18eddaffb32f
serikoffBy <kornaush@gmail.com>
2020-10-03 22:52:06 +0300

    fix: update function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if (!members || members.length === 0 ||) return false;
+  if (!members || members.length === 0) return false;
   if (!Array.isArray(members)) return false;
   let result = [];
   members.forEach(element => {
