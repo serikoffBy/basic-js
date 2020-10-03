6bd11a1dddbadaf8e3ce815b8d50526057f87b65
serikoffBy <kornaush@gmail.com>
2020-10-04 01:41:32 +0300

    fix: update function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -7,7 +7,7 @@ module.exports = function dateSample(sampleActivity) {
   if(arguments.length === 0 || typeof sampleActivity !== 'string' || sampleActivity.length === 0)
     return false;
   
-  if ((parseFloat(sampleActivity) < 1 && parseFloat(sampleActivity) > 15) || Number.isNaN(parseFloat(sampleActivity))) return false;
+  if ((parseFloat(sampleActivity) < 1 || parseFloat(sampleActivity) > 15) || Number.isNaN(parseFloat(sampleActivity))) return false;
   else {
     const k = 0.693/HALF_LIFE_PERIOD;
     return Math.ceil((Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / k);
