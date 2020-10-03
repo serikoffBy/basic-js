1af09b1d9057f440c4a9503982767204ceaf3cdc
serikoffBy <kornaush@gmail.com>
2020-10-03 17:41:31 +0300

    init: create function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,11 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  throw new CustomError('Not implemented');
+  if (!members) return false;
   let result = [];
   members.forEach(element => {
     if(typeof(element) === "string") {
-      result.push(result.trim()[0].toUpperCase());
+      result.push(element.trim()[0].toUpperCase());
     }
   });
   return result.sort().join('');
