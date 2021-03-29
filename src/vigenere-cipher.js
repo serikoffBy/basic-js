9c53754101a70def6c4bddcd5475f83c84bbcc93
serikoffBy <kornaush@gmail.com>
2021-03-29 13:14:06 +0300

    init: start vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type=true) {
+    this.type = type;
+  }
+  
+  encrypt(message, key, type) {
+    const alphabit = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result='';
+    let newKey='';
+    let newMessage = message.toUpperCase();
+    if(key.length < message.length) {
+      newKey = (key.padEnd(message.length, key)).toUpperCase();
+    }
+    else {
+      newKey = key.toUpperCase();
+    }
+    for(let i = 0, j = 0; i < newMessage.length; i++, j++) {
+      if(alphabit.includes(newMessage[i])) {
+        result = result + alphabit[(alphabit.indexOf(newMessage[i]) + alphabit.indexOf(newKey[j])) % alphabit.length];
+      }
+      else {
+        result = result + newMessage[i];
+        j--;        
+      }
+    }
+    return this.type ? result : result.split('').reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key, type) {
+    const alphabit = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result='';
+    let newKey='';
+    let newMessage = message.toUpperCase();
+    if(key.length < message.length) {
+      newKey = (key.padEnd(message.length, key)).toUpperCase();
+    }
+    else {
+      newKey = key.toUpperCase();
+    }
+    for(let i = 0, j = 0; i < newMessage.length; i++, j++) {
+      if(alphabit.includes(newMessage[i])) {
+        result = result + alphabit[(alphabit.indexOf(newMessage[i]) + alphabit.length - alphabit.indexOf(newKey[j])) % alphabit.length];
+      }
+      else {
+        result = result + newMessage[i];
+        j--;        
+      }
+    }
+    return this.type ? result : result.split('').reverse().join('');
   }
 }
 
